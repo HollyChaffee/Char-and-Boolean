c16d1e7c93c500fb6476cd66401e471a4f9791a0
Holly Chaffee <saltdc95@gmail.com>
2020-11-30 14:21:59 -0500

    First commit

---
+++ src/com/hollychaffee/Main.java
@@ -0,0 +1,23 @@
+package com.hollychaffee;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        char myChar = 'H';
+        char myUnicodeChar = '\u0048';
+        char myCopyrightChar = '\u00A9';
+        System.out.println(myChar);
+        System.out.println(myUnicodeChar);
+        System.out.println(myCopyrightChar);
+
+        boolean myTrueBooleanValue = true;
+        boolean myFalseBooleanValue = false;
+        boolean isCustomerOverTwentyOne = true;
+
+        System.out.println(myTrueBooleanValue);
+        System.out.println(myFalseBooleanValue);
+        System.out.println(isCustomerOverTwentyOne);
+
+    }
+}
